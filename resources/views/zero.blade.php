99efa3a1d40ed944c0ec3ece3d399226b3476723
nagyszem22 <takacsmarton22@gmail.com>
2017-06-21 11:44:10 +0200

    changed view port on the very first page

--- resources/views/zero.blade.php
+++ resources/views/zero.blade.php
@@ -3,6 +3,9 @@
     <head>
         <title>The Bogi</title>
 
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <link href="https://fonts.googleapis.com/css?family=Merriweather:400,700,900&amp;subset=latin-ext" rel="stylesheet">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="{{ asset('main.css') }}" crossorigin="anonymous">
